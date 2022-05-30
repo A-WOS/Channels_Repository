34c0caffc75345e3c45e9c75fa4a80e29a7ed5bd
AWOS <uws0331@gmail.com>
2022-05-30 16:38:18 +0900

    웹 소켓 비동기에서 동기로

--- chat/consumer.py
+++ chat/consumer.py
@@ -1,50 +1,30 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
 
 
-# class ChatConsumer(WebsocketConsumer):
-#     def connect(self):
-#         self.accept()
-#
-#     def disconnect(self, close_code):
-#         pass
-#
-#     def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-#
-#         self.send(text_data=json.dumps({
-#             'message': message
-#         }))
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # "room" 그룹에 가입
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        # "room" 그룹에서 탈퇴
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    # 웹소켓 으로 부터 메시지 수신
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        # "room" 그룹에 메시지 전송
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -52,11 +32,9 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    # "room" 그룹에서 메시지 전송
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
-        # 웹 소켓으로 메시지 전송
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
