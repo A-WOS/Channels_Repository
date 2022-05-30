ae89a41a301295bdb9528d8bb933890b9da86d81
AWOS <uws0331@gmail.com>
2022-05-30 16:05:57 +0900

    - add routing, websocket

--- mysite/routing.py
+++ mysite/routing.py
@@ -1,5 +1,18 @@
-from channels.routing import ProtocolTypeRouter
+import django.urls
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
+
+# from channels.routing import ProtocolTypeRouter
+#
+# application = ProtocolTypeRouter({
+#     # (http->django views is added by default)
+# })
